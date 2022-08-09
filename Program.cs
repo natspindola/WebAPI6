84fbd0acbc2cff45e2ab564cbf9d953f0cdd0981
Natália Spindola <natspindola@hotmail.com>
2022-08-09 16:32:19 -0300

    Configurando ambientes

--- Program.cs
+++ Program.cs
@@ -2,24 +2,23 @@ using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
 
 app.MapGet("/", () => "Hello World!");
-app.MapPost("/", () => new {Name = "Natalia Spindola", Age = 26});
-app.MapGet("/AddHeader", (HttpResponse response) => {
-    response.Headers.Add("Teste", "Natalia Spindola");
-    return new {Name = "Natalia Spindola", Age = 26};
-});
 
 app.MapPost("/products", (Product product) => {
     ProductRepository.Add(product);
     return Results.Created($"/products/{product.Code}", product.Code);
 });
 
-//api.app.com/users/{code} -> passando informações pela rota
 app.MapGet("/products/{code}", ([FromRoute] string code) => {
     var product = ProductRepository.GetBy(code);
-    if(product != null)
+    if(product != null) {
+        Console.WriteLine("Product found");
         return Results.Ok(product);
+    }
+        
     return Results.NotFound();
 });
 
@@ -35,26 +34,22 @@ app.MapDelete("/products/{code}", ([FromRoute] string code) => {
     return Results.Ok();
 });
 
-
-// //api.app.com/users?datastart={date}&dataend={date} -> PASSANDO INFORMAÇÕES PELO QUERY, ENDPOINTS, URL
-// app.MapGet("/getproduct", ([FromQuery] string dateStart, [FromQuery] string dateEnd) => {
-//     return dateStart + " - " + dateEnd;
-// });
-
-// //PASSANDO INFORMAÇÕES PELO HEADER
-// app.MapGet("/getproductbyheader", (HttpRequest request) => {
-//     return request.Headers["product-code"].ToString();
-// });
+if(app.Environment.IsStaging()) //roda apenas no ambiente staging
+app.MapGet("/configuration/database", (IConfiguration configuration) => {
+    return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+});
 
 app.Run();
 
 public static class ProductRepository {
-    public static List<Product> Products { get; set; }
+    public static List<Product> Products { get; set; } = Products = new List<Product>();
+
+    public static void Init(IConfiguration configuration) {
+        var products = configuration.GetSection("Products").Get<List<Product>>();
+        Products = products;
+    }
 
     public static void Add(Product product) {
-        if(Products == null)
-            Products = new List<Product>();
-
         Products.Add(product);
     }
 
